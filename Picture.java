66be7414eaacd6ddae4a15df62d405844e00a85b
PaoloCalda <g00303652@stu.raritanval.edu>
2023-01-23 12:37:15 -0500

    Face

--- Picture.java
+++ Picture.java
@@ -6,15 +6,20 @@
  *
  * This class was written as an early example for teaching Java with BlueJ.
  * 
- * @author  Michael Kšlling and David J. Barnes
+ * @author  Paolo Caldaro
  * @version 2016.02.29
  */
 public class Picture
 {
     private Square wall;
-    private Square window;
+    private Square door;
+    private Square shed;
     private Triangle roof;
+    private Triangle shedroof;
     private Circle sun;
+    private Person man;
+    private Square eye;
+    private Square mouth;
     private boolean drawn;
 
     /**
@@ -23,9 +28,14 @@ public class Picture
     public Picture()
     {
         wall = new Square();
-        window = new Square();
-        roof = new Triangle();  
+        mouth = new Square();
+        eye = new Square();
+        door = new Square();
+        shed = new Square();
+        roof = new Triangle();
+        shedroof = new Triangle();
         sun = new Circle();
+        man = new Person();
         drawn = false;
     }
 
@@ -35,27 +45,58 @@ public class Picture
     public void draw()
     {
         if(!drawn) {
-            wall.moveHorizontal(-140);
-            wall.moveVertical(20);
-            wall.changeSize(120);
+            wall.moveHorizontal(-180);
+            wall.moveVertical(-40);
+            wall.changeSize(130);
             wall.makeVisible();
             
-            window.changeColor("black");
-            window.moveHorizontal(-120);
-            window.moveVertical(40);
-            window.changeSize(40);
-            window.makeVisible();
-    
+            mouth.moveHorizontal(-100);
+            mouth.moveVertical(160);
+            mouth.changeSize(130);
+            mouth.makeVisible();
+            
+            shed.changeColor("red");
+            shed.moveHorizontal(-10);
+            shed.moveVertical(-40);
+            shed.changeSize(130);
+            shed.makeVisible();
+            
+            door.changeColor("black");
+            door.moveHorizontal(-140);
+            door.moveVertical(0);
+            door.changeSize(50);
+            door.makeVisible();
+            
+            eye.changeColor("black");
+            eye.moveHorizontal(30);
+            eye.moveVertical(0);
+            eye.changeSize(50);
+            eye.makeVisible();
+            
+            roof.changeColor("blue");
             roof.changeSize(60, 180);
-            roof.moveHorizontal(20);
-            roof.moveVertical(-60);
+            roof.moveHorizontal(-25);
+            roof.moveVertical(-110);
             roof.makeVisible();
-    
-            sun.changeColor("yellow");
-            sun.moveHorizontal(100);
-            sun.moveVertical(-40);
+            
+            shedroof.changeColor("blue");
+            shedroof.changeSize(60, 180);
+            shedroof.moveHorizontal(150);
+            shedroof.moveVertical(-110);
+            shedroof.makeVisible();
+            
+            sun.changeColor("blue");
+            sun.moveHorizontal(10);
+            sun.moveVertical(100);
             sun.changeSize(80);
             sun.makeVisible();
+            
+            man.changeColor("black");
+            man.changeSize(20, 20);
+            man.moveHorizontal(0);
+            man.moveVertical(65);
+            man.makeVisible();
+    
             drawn = true;
         }
     }
@@ -66,7 +107,7 @@ public class Picture
     public void setBlackAndWhite()
     {
         wall.changeColor("black");
-        window.changeColor("white");
+        door.changeColor("white");
         roof.changeColor("black");
         sun.changeColor("black");
     }
@@ -77,7 +118,7 @@ public class Picture
     public void setColor()
     {
         wall.changeColor("red");
-        window.changeColor("black");
+        door.changeColor("black");
         roof.changeColor("green");
         sun.changeColor("yellow");
     }
